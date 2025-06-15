3d443d3b1a6fc62eae5b855683b29b11a7f68c27
GraceChen130 <122394438+GraceChen130@users.noreply.github.com>
2025-06-16 07:00:56 +0800

    add readme and 2 test scripts

---
+++ calculator.py
@@ -0,0 +1,8 @@
+def add_two_numbers(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+def subtract_two_numbers(a, b):
+    """Returns the difference of two numbers."""
+    return a - b
+
