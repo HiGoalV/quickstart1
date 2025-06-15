4d97fa1b5d0d3cce132fb60307cfd7efde6dc9da
GraceChen130 <122394438+GraceChen130@users.noreply.github.com>
2025-06-16 07:18:31 +0800

    update calculator include multiple

--- calculator.py
+++ calculator.py
@@ -6,3 +6,6 @@ def subtract_two_numbers(a, b):
     """Returns the difference of two numbers."""
     return a - b
 
+def multiply_two_numbers(a, b):
+    """Returns the product of two numbers."""
+    return a ** b
